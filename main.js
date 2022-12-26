daf41dc7d076a8c75c6ce711a82ac1dd97b87767
JKuriata <120460338+JKuriata@users.noreply.github.com>
2022-12-26 16:51:16 +0000

    Persist dark mode across pages
    Will also persist through page reloads

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
+const darkModeClassLabel = "dark-mode";
+const darkModeStorageLabel = "dark-mode-enabled";
+
+const isDarkModeEnabled = localStorage.getItem(darkModeStorageLabel) ?? 'false';
+if (isDarkModeEnabled === 'true') {
+  document.body.classList.add(darkModeClassLabel);
+}
+
 function DarkMode() {
     var element = document.body;
-    element.classList.toggle("dark-mode");
+    element.classList.toggle(darkModeClassLabel);
+
+    const isDarkModeEnabled = element.classList.contains(darkModeClassLabel);
+    localStorage.setItem(darkModeStorageLabel, isDarkModeEnabled);
   }
